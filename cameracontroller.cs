c86febab5b6af65100d6b3519167707e426c944d
HemanthDonga <42300849+HemanthDonga@users.noreply.github.com>
2020-05-17 22:07:49 +0530

    Add files via upload

---
+++ cameracontroller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameracontroller : MonoBehaviour
+{
+    public Transform ObjectToFollow;
+    public Vector3 offset;
+    public float viewingspeed = 10;
+    public float followspeed = 10;
+
+    public void looktotarget()
+    {
+        Vector3 _lookdirection = ObjectToFollow.position - transform.position;
+        Quaternion _rotat = Quaternion.LookRotation(_lookdirection, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, _rotat, viewingspeed * Time.deltaTime);
+    }
+    public void movetowardtarget()
+    {
+        Vector3 _targetPos = ObjectToFollow.position +
+                             ObjectToFollow.forward * offset.z +
+                             ObjectToFollow.right * offset.x +
+                             ObjectToFollow.up * offset.y;
+
+        transform.position = Vector3.Lerp(transform.position, _targetPos, followspeed * Time.deltaTime);
+    }
+
+    public void FixedUpdate()
+    {
+        looktotarget();
+        movetowardtarget();
+    }
+}
---
+++ controller.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour
+{
+
+    public bool ground;
+    public Rigidbody rb;
+    public Vector3 movement;
+    public float horizontal;
+    public float vertical;
+    public float jump;
+    public float jumpheight = 9f;
+    public Transform sphereobject;
+    public float speed;
+    public float max_speed = 10f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        ground = true;
+        
+    }
+
+    // Update is called once per frame
+
+    private void FixedUpdate()
+    {
+        sphereinput();
+        sphereaction();
+    }
+
+    private void OnCollisionEnter(Collision objectsphere)
+    {
+        objectsphere.gameObject.CompareTag("ground");
+        ground = true;
+    }
+
+    void sphereinput()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        jump = Input.GetAxis("Jump");
+
+    }
+
+    void sphereaction()
+    {
+        speed = max_speed;
+        movement = new Vector3(1f * speed * horizontal*Time.deltaTime, 1f * jumpheight * jump * Time.deltaTime, 1f * speed * vertical * Time.deltaTime);
+        rb.MovePosition(transform.position + movement);
+        ground = false;
+    }
+
+}
